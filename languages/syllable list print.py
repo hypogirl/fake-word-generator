072dbc07a9a0f06da272c648b135688b99b0806e
joão <74837451+slocknad@users.noreply.github.com>
2021-10-11 14:08:49 +0100

    Update syllable list print.py

--- languages/syllable list print.py
+++ languages/syllable list print.py
@@ -1,25 +1,34 @@
+import itertools
 from portuguese import pt
-from russian import ru
+def vowel(l):
+    return l in ["a","e","i","o","u","á","à","â","ǎ","ă","ã","ả","ȧ","ạ","ä","å","é","è","ê","ḙ","ě","ĕ","ẽ","ḛ","ẻ","ė","ë","ē","ȩ","ę","í","ì","ĭ","î","ǐ","ï","ḯ","ĩ","į","ī","ó","ò","ŏ","ô","ố","ồ","ỗ","ổ","ǒ","ö","ȫ","ő","õ","ú","ù","ŭ","û","ǔ","ů","ü","ǘ","ǜ","ǚ","ǖ","ű",'я','o','ё','ю','у','э','е','и','ы']
 
-language = ["one of the languages"]
-
+language = pt
 syllable_dictionary = {}
 
+lastletter = None
 for word in language:
+    if lastletter:
+        print(syllable)
+        if syllable in syllable_dictionary:
+            syllable_dictionary[syllable] += 1
+        else:
+            syllable_dictionary[syllable] = 1
+        syllable = str()
+        lastletter = None
     syllable = str()
-    lastletter = None
     for letter in word:
         if vowel(lastletter) and not vowel(letter):
+            print(syllable)
             if syllable in syllable_dictionary:
                 syllable_dictionary[syllable] += 1
             else:
                 syllable_dictionary[syllable] = 1
+            syllable = str()
+            lastletter = None
+        syllable += letter
         lastletter = letter
 
 syllable_dictionary_sorted = dict(sorted(syllable_dictionary.items(), key=lambda item: item[1]))
 
-i = 1
-print(list(syllable_dictionary_sorted)[:100])
-
-def vowel(l):
-    return l in ["á","à","â","ǎ","ă","ã","ả","ȧ","ạ","ä","å","é","è","ê","ḙ","ě","ĕ","ẽ","ḛ","ẻ","ė","ë","ē","ȩ","ę","í","ì","ĭ","î","ǐ","ï","ḯ","ĩ","į","ī","ó","ò","ŏ","ô","ố","ồ","ỗ","ổ","ǒ","ö","ȫ","ő","õ","ú","ù","ŭ","û","ǔ","ů","ü","ǘ","ǜ","ǚ","ǖ","ű",'я','o','ё','ю','у','э','е','и','ы']
+print(dict(itertools.islice(syllable_dictionary_sorted.items(), 100)))
